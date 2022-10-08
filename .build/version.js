c034b25eef35037a66248e0df3f21adb1ae75789
Alexander Heimbuch <github@heimbu.ch>
2022-10-08 09:37:48 +0200

    fix: version bump

--- .build/version.js
+++ .build/version.js
@@ -1,13 +1,18 @@
 const replace = require("replace-in-file");
 const ENTRY = "podlove-player-aggregator.php";
 const version = require("../package.json").version;
-const semverRegex = /(?<=^v?|\sv?)(?:(?:0|[1-9]\d{0,9}?)\.){2}(?:0|[1-9]\d{0,9})(?:-(?:--+)?(?:0|[1-9]\d*|\d*[a-z]+\d*)){0,100}(?=$| |\+|\.)(?:(?<=-\S+)(?:\.(?:--?|[\da-z-]*[a-z-]\d*|0|[1-9]\d*)){1,100}?)?(?!\.)(?:\+(?:[\da-z]\.?-?){1,100}?(?!\w))?(?!\+)/gi;
 
 async function main() {
   await replace({
     files: ENTRY,
-    from: semverRegex,
-    to: version,
+    from: /(Version:           \d\.\d\.\d[-]?[\d])/,
+    to: `Version:           ${version}`,
+  });
+
+  await replace({
+    files: ENTRY,
+    from: /(define\( \'PODLOVE_PLAYER_AGGREGATOR_VERSION\'\, \'\d\.\d\.\d[-]?[\d]\' \))/,
+    to: `define( 'PODLOVE_PLAYER_AGGREGATOR_VERSION', '${version}' )`,
   });
 }
 
